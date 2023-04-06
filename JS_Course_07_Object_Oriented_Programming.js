3ffc6112d98182956134d735d5ad8d31981fd5c4
Dasaavawar <67240388+Dasaavawar@users.noreply.github.com>
2023-04-06 15:12:10 -0500

    Update JS_Course_07_Object_Oriented_Programming.js

--- JS_Course_07_Object_Oriented_Programming.js
+++ JS_Course_07_Object_Oriented_Programming.js
@@ -1,6 +1,13 @@
 /* freeCodeCamp - JavaScript Algorithms and Data Structures - Courses - VII - Object Oriented Programming */
 
 
+/*
+OOP, or Object Oriented Programming, is one of the major approaches to the software development process. In OOP, objects and classes organize code to describe things and what they can do.
+
+In this course, you'll learn the basic principles of OOP in JavaScript, including the this keyword, prototype chains, constructors, and inheritance.
+*/
+
+
 /* 1. Object Oriented Programming - Create a Basic JavaScript Object */
 /*
 Think about things people see every day, like cars, shops, and birds. These are all objects: tangible things people can observe and interact with.
